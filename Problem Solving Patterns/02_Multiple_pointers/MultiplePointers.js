58c5f604ce6194a61abbb02ca5be808b7935a4be
Yash <yashdesh095@gmail.com>
2024-02-18 15:31:02 +0530

    solved another problem countUniqueValues

--- Problem Solving Patterns/02_Multiple_pointers/MultiplePointers.js
+++ Problem Solving Patterns/02_Multiple_pointers/MultiplePointers.js
@@ -16,3 +16,11 @@ Question :- Write a function called sumZero which accepts  a sorted array of int
             sumZero ([1,2,3]) //     undefined
 
 */
+/* 
+Question :- Implement a function count unique values ,which accepts a sorted array and counts the unique 
+            values in the array.There can be negative numbers in the array,but it will always be sorted.
+
+            countUniqueValues([1,1,1,1,2]) // 2
+            countUniqueValues([1,2,3,4,4,4,7,7,12,12]) // 7
+            countUniqueValues([-2,-1,-1,0,1]) // 2
+*/
