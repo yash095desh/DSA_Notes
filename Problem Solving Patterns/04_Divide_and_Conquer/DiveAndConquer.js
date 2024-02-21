69a317c7a16c2561134e1e7d6febf047c2dcd343
Yash <yashdesh095@gmail.com>
2024-02-21 09:58:33 +0530

    learned Divide and Conquer

---
+++ Problem Solving Patterns/04_Divide_and_Conquer/DiveAndConquer.js
@@ -0,0 +1,17 @@
+/*
+Divide And Conquer :-
+                        This Pattern involves dividing a data set into smaller chunks and then repeating a process 
+                        a set into smaller chunks and then  repeating a process with a subset of data 
+
+                        * this pattern can tremendously decrese time complexity
+                        self exp :-(
+                           - This pattern is frequntly used in sorting algorithem reduces the time 
+                            complexity O(n) to O(logn) 
+                           - in this pattern we divide array in half and then checks the values lies in which part
+                            (or check that is required value is greater than or less than mid value )
+                            and thus using this repetedly we get the required value in much less time 
+                           - helpful with dealing with large arrays 
+                        )
+
+*/
+
