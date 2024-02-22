431b1b25c7fa2d1f3fea3f85a1e71a233cc661f9
Yash <yashdesh095@gmail.com>
2024-02-22 11:47:04 +0530

    solution updated

--- Problem Solving Patterns/05_Problem/Qusetions.js
+++ Problem Solving Patterns/05_Problem/Qusetions.js
@@ -181,32 +181,31 @@ Time Complexity - O(n)
 
 Space Complexity - O(1)
   */
-
-function minSubArrayLen(arr,num){
-
-    let shortest = Infinity
-    let temp = []
-    let sum = 0;
-    let i =0
-    let j = 0
-    while(j<arr.length){
-      if(sum < num){
-        temp.push(arr[i])
-        sum = sum + arr[i]
-        i++
-      }
-      else if (sum>= num){
-        sum = sum - arr[j]
-        temp = temp.slice(j,temp.length)
-        console.log(temp)
-        shortest = Math.min(shortest,temp.length)
-        j++
-      }
-      else{
-        break
-      }
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then 
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
     }
-    return shortest
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
+  }
+ 
+  return minLen === Infinity ? 0 : minLen;
 }
-
-console.log(minSubArrayLen([2,3,1,2,4,3], 7))
