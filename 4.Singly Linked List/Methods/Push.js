74de5a2054a43d3ffbd60f6915509b5988dee6be
Yash <yashdesh095@gmail.com>
2024-03-03 18:06:39 +0530

    Singly Linked list notes added

--- 4.Singly Linked List/Methods/Push.js
+++ 4.Singly Linked List/Methods/Push.js
@@ -1,3 +1,19 @@
+// Pushing Pseodocode
+
+/*
+    - This function accept a value 
+    - Creates a new node using the value passed to the function
+    - If there is no head property on the list ,set the head and tail 
+        to be the newly created node 
+    - Otherwise set the next property on the tail to be the newnode 
+        and set the tail property on the list to be the newly created node 
+    - Increment the length by one 
+
+*/
+
+
+
+
 class Node {
     constructor(value){ 
         this.value = value,
---
+++ 4.Singly Linked List/SinglyLinkedList.js
@@ -0,0 +1,29 @@
+//
+/*
+    OBJECTIVES
+    - Define what a Singly Linked List is 
+    - Compare and Contrast Linked List with Arrays 
+    - Implement insertion removal and transversal methods on singly Linked Lists
+    - Transversal methods on Singly linked list 
+*/
+/*
+    What is a Linked List ? 
+    A data structure that contains a head ,tail , and length property 
+
+    linked list consits of nodes and each node has a value and a pointer
+    to another node or null
+
+*/
+/*
+    Comparisons with Array 
+    List : 
+        - Do Not have indexes
+        - Connected via nodes with a pointer
+        - Random access is not allowed 
+
+    Arrays 
+        - Indexed in order ! 
+        - Insertion and deletion can be expensive
+        - Can quickly be accessed at a specific  index
+        
+*/
