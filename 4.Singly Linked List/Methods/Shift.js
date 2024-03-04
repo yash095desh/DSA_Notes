1679517955221681af137f93f5322fe25061d4e2
Yash <yashdesh095@gmail.com>
2024-03-04 10:15:32 +0530

    completed shifting pseudocode

---
+++ 4.Singly Linked List/Methods/Shift.js
@@ -0,0 +1,12 @@
+// Shifting : 
+
+/* Removing a new node from the beginning of the linked list  
+
+# Shifting Pseudocode 
+    - If there are no nodes return undefined
+    - Store the current head property in a variable
+    - Set the head property to be the current head next property 
+    - decrement the length by 1
+    - Return the value of the node removed
+*/
+
