e48fd9de642edb8d8022b4eb4f816cb26e64a4ca
Yash <yashdesh095@gmail.com>
2024-03-04 10:43:33 +0530

    added Unshif pseudocode

---
+++ 4.Singly Linked List/Methods/Unshift.js
@@ -0,0 +1,13 @@
+// UnShifting
+
+/*
+    Adding a new node to the beginning of the linked list 
+
+    # Unshifting Pseudocode
+    - This function should accept a value 
+    - Create a new node using the value passed to the function 
+    - If there is no head property on the list set head and tail to be the newly createed one 
+    - Otherwise set the newly created nodes next property to be the current head property 
+    - Set the head property on the list to be that newly created node 
+    - Increment the length of the list by 1 and return the list 
+*/
