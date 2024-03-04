9e64b4740276f1d77eb110125132bfd9fbebca1f
Yash <yashdesh095@gmail.com>
2024-03-04 10:22:07 +0530

    added shift method

--- 4.Singly Linked List/Methods/Shift.js
+++ 4.Singly Linked List/Methods/Shift.js
@@ -10,3 +10,80 @@
     - Return the value of the node removed
 */
 
+
+class Node {
+    constructor(value){ 
+        this.value = value,
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.length = 0,
+        this.head = null,
+        this.tail = null
+    }
+    push(value){
+        let newnode = new Node(value)
+        if(!this.head){
+            this.head = newnode
+            this.tail = newnode
+        }else{
+            this.tail.next = newnode
+            this.tail = newnode
+        }
+        this.length ++
+    }
+    transverse(){
+        let current = this.head
+        while(current){
+            console.log(current.value)
+            current = current.next
+        }
+    }
+    pop(){
+        if(!this.head){
+            return undefined
+        }
+        let current = this.head
+        let newtail = current
+        while(current.next){
+            newtail = current
+            current = current.next
+        }
+        newtail.next = null
+        this.tail = newtail
+        this.length--
+        if(this.length === 0){
+            this.head = null,
+            this.tail = null
+        }
+        return current.value
+    }
+    shift(){
+        if(!this.head){
+            return undefined
+        }
+        let prev = this.head
+        this.head = this.head.next
+        this.length--
+        return prev.value
+    }
+}
+
+let list = new SinglyLinkedList
+list.push("hi")
+list.push("there")
+list.push("What")
+list.push("are")
+list.push("You")
+list.push("doing")
+list.shift()
+list.shift()
+list.shift()
+list.shift()
+console.log(list.shift())
+console.log(list.shift())
+console.log(list.shift())
+list.transverse()
