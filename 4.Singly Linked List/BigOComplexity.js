23547279965a7bd2c42a5f916553940f363acee1
Yash <yashdesh095@gmail.com>
2024-03-06 17:41:37 +0530

    learned about Big O complexity in Singlylinked list

---
+++ 4.Singly Linked List/BigOComplexity.js
@@ -0,0 +1,18 @@
+// BIG O COMPLEXITY
+
+/*
+Insertion : O(1)
+Removal : It depends ....O(1) or O(N)
+Searching :O(N)
+Access :O(N)
+*/
+
+//Recap 
+/*
+a) Singly Linked list are an excellent alternative to arrays when insertion and deletion at the beggining are
+    frequently required
+b) Arrays contain a build in index whereas Linked list do not 
+c) The idea of a linked list data structure that consists of nodes is the foundation 
+ for other data structures like Stacks and Queues
+ 
+*/
