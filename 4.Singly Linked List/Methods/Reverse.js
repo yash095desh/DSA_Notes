7c4c17ce538dee218d50a51bcc984d109c5ede4e
Yash <yashdesh095@gmail.com>
2024-03-06 11:25:06 +0530

    reverse pseudocode completed

---
+++ 4.Singly Linked List/Methods/Reverse.js
@@ -0,0 +1,13 @@
+// REVERSE METHOD
+
+/*
+ : Reverse the linked list in place
+  pseudocode
+  1) Swap the head and tail
+  2) Create a variables called next , prev
+  3) Create a variable called node and initialized it to the head property 
+  4)loop through the list 
+  5)Set next to be the next property on whatever node is 
+  6) Set prev to be the value of the node variable
+  7) set the node variable to be the value of the next variable 
+*/
