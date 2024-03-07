844b2de3095fadc5761984aa785cb9fa4d5ba8b4
Yash <yashdesh095@gmail.com>
2024-03-07 10:13:10 +0530

    added notes for doubly linked list

---
+++ 5.Doubly Linked List/DoublyLinkedList.js
@@ -0,0 +1,18 @@
+// Doubly Linked List 
+
+//Objectives
+/*
+1)Construct  a Doubly Linked List 
+2)Compare and contrast Doubly and Singly Linked List 
+3)Implement basic Operation on Doubly Linked List  
+*/
+
+/*
+# DOUBLY LINKED LIST :
+                        Almost identical to Singly Linked List ,
+                        expect every node has another pointer ,to the 
+                        previous node
+
+# Comparing with Singly linked List 
+    takes more memory  ==== More Flexiblity
+*/
