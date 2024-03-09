8040df424e392d01b3762c4c56f36ae15f5c2219
Yash <yashdesh095@gmail.com>
2024-03-09 18:31:23 +0530

    Unshift Method Added

--- 5.Doubly Linked List/Methods/UnShift.js
+++ 5.Doubly Linked List/Methods/UnShift.js
@@ -16,3 +16,88 @@
     5) Return the List 
 */
 
+class Node {
+    constructor(val){
+        this.value = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class DoublyLinkedlist {
+    constructor (){
+        this.head = null;
+        this.tail = null;
+        this.length = 0
+    }
+    push(val){
+        let newnode = new Node(val)
+        if(!this.head){
+            this.head = newnode
+            this.tail = newnode
+        }
+        else{
+            this.tail.next = newnode
+            newnode.prev = this.tail
+            this.tail = newnode
+        }
+        this.length++
+        return this
+    }
+    transverse(){
+        let arr = []
+        let node = this.head
+        for (let i = 0; i < this.length; i++) {
+            arr.push(node.value)
+            node = node.next
+        }
+        return arr
+    }
+    pop(){
+        if(!this.head)return undefined
+        let removedtail = this.tail
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            this.tail = removedtail.prev
+            this.tail.next = null
+            removedtail.prev = null
+        }
+            this.length--;
+            return removedtail
+    }
+    shift(){
+        if(!this.head)return undefined
+        let oldhead = this.head
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        else{
+        this.head = oldhead.next;
+        this.head.prev = null
+        oldhead.next = null
+        this.length--
+        }
+        return oldhead
+    }
+    unshift(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this
+    }
+}
+let list = new DoublyLinkedlist()
+list.push('Hello!')
+list.push('there')
+list.push('what')
+console.log(list.unshift('Hi'))
+console.log(list.transverse())
