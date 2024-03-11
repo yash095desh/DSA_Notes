01cb0e5a3f61155566cbdeeae1910e7a3e7c8a09
Yash <yashdesh095@gmail.com>
2024-03-11 18:55:37 +0530

    added Big O notation and Recap of Stacks

--- 6.Stacks/Introduction.js
+++ 6.Stacks/Introduction.js
@@ -20,4 +20,16 @@
         - Managing function invocations
         - Undo/Redo 
         - Routing (the history object)
+
+    * Big O notations for stack 
+        - Insertion O(1)
+        - Removal O(1)
+        - Searching O(N)
+        - Access O(N)
+    
+    * RECAP 
+        -Stack are a LIFO structure where the last value in is always first out
+        -Stacks are used to handle function invocations (the call stack) ,for operation like undo/redo and for 
+         routing (remember pages you have visited and go back/forward) and much more
+        - They are not a built in data structure is Javascript but are relatively simple to implement 
 */
