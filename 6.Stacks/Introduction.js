a14f3a704959350077fb07fe954a725e43490340
Yash <yashdesh095@gmail.com>
2024-03-11 12:38:35 +0530

    Stacks Introduction

---
+++ 6.Stacks/Introduction.js
@@ -0,0 +1,23 @@
+// Stacks 
+
+/*
+    Objectives:-
+    a)Define what a Stack is 
+    b)Understand use cases for stack 
+    c)Implement operations on a stack 
+    d)Data Structure
+
+    * What is a Stack ?
+    -> A LIFO Data Structure
+        the last element added to the stack will be the first element removed from 
+        the sack 
+    
+    * How is it used ?
+    -> Think about a stack of plates , or stack of markers ,or stack of 
+        anything As you pile it up the last (or the topmost thing ) is what gets removed first 
+    
+    * Where the Stacks are used
+        - Managing function invocations
+        - Undo/Redo 
+        - Routing (the history object)
+*/
