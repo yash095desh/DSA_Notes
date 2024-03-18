9dcd6371a06fb35769380cb46ac7f2a2f2c6b1ed
Yash <yashdesh095@gmail.com>
2024-03-18 12:54:33 +0530

    added searching notes

---
+++ Searching Algorithems/Searching.js
@@ -0,0 +1,14 @@
+// # Searching
+/*
+Searching is the fundamental process of locating a specific element or item within a collection of data This  collection 
+of data can be take various forms ,such as arrays , lists trees or other structured representaions
+*/
+
+// Objectives 
+/*
+- Describe what a searching algorithem is 
+- Implement linear Search on arrays 
+- Implement binary Search on sorted arrays 
+- Implement a native string searching algorithem
+- Implement the KMP string searching algorithem
+ */
